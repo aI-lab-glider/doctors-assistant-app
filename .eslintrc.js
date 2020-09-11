5b772fece74c0b21cc2c46ad8069ddf20e3bebcc
Tomasz Pecak <pecatoma@gmail.com>
2020-09-11 23:37:49 +0200

    Enable console logs

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
         extensions: [".js", ".jsx"],
       },
     ],
+    "no-console": "off",
     "no-use-before-define": ["error", { variables: false }],
     "prettier/prettier": ["error", prettierConfig],
     "prefer-template": 2,
