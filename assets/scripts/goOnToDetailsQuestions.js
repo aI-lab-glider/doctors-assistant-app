5a391f65b1ea7d05f4b08a99e80866808cebf1c5
alicjak1519 <akrzeminska1519@gmail.com>
2020-09-26 15:24:39 +0200

    Improve scripts to diagnosis

--- assets/scripts/goOnToDetailsQuestions.js
+++ assets/scripts/goOnToDetailsQuestions.js
@@ -1,11 +1,11 @@
 /**
  * Decide about flow going on to details question
+ * @param {String} module_code
  * @param {Array} major_answers - Array with major questions answers
- * @param {Array} min_major_true - Array with minor questions answers
+ * @param {Object} modules - Array with minor questions answers
  * @return {Boolean} - True if flow shoud go on details questions
  */
 
-function goOnToDetailsQuestions(major_answers,
-                                min_major_true) {
-  return (major_answers.reduce((a, b) => a + b, 0) >= min_major_true)
+function goOnToDetailsQuestions(module_code, major_answers ,modules) {
+  return (major_answers.reduce((a, b) => a + b, 0) >= min_major_true_answers)
 }
