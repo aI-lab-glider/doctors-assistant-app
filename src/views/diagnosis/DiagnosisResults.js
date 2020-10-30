10649f233d305f03e14c8ae9ae5a0b5bf73fd374
mon8-lig <mon8-lig@o2.pl>
2020-10-30 13:38:06 +0100

    Support for diagnosis results (#33)

--- src/views/diagnosis/DiagnosisResults.js
+++ src/views/diagnosis/DiagnosisResults.js
@@ -1,25 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FlatList } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
 import Result from "../../components/diagnosis/Result";
 import DiagnosisContainer from "./DiagnosisContainer";
 import {
   diseasesProbabilityPropTypes,
   modulePropTypes,
 } from "../../constants/propTypes/diagnosis";
+import { Colors, Typography } from "../../constants/styles";
 
 const DiagnosisResults = ({ route }) => {
+  const threshold = 0.33;
   const { diseasesProbability, module } = route.params;
+  const filteredDiseasesProbability = diseasesProbability
+    .filter((diseaseProbability) => {
+      return diseaseProbability.probability > threshold;
+    })
+    .sort((a, b) => parseFloat(b.probability) - parseFloat(a.probability));
+
   return (
     <DiagnosisContainer module={module} subTitle="Podsumowanie">
-      <FlatList
-        data={diseasesProbability}
-        renderItem={({ item }) => <Result onPress={() => {}} item={item} />}
-        keyExtractor={(item, index) => index.toString()}
-      />
+      {filteredDiseasesProbability.length > 0 && (
+        <FlatList
+          data={filteredDiseasesProbability}
+          renderItem={({ item }) => <Result onPress={() => {}} item={item} />}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      )}
+      {filteredDiseasesProbability.length === 0 && (
+        <Text style={styles.text}>
+          Brak chorób o prawdopodobieństwie większym niż {threshold * 100}%
+        </Text>
+      )}
     </DiagnosisContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    fontSize: Typography.FONT_SIZE_14,
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    color: Colors.PURPLE,
+    paddingLeft: 10,
+  },
+});
 
 DiagnosisResults.propTypes = {
   route: PropTypes.shape({
